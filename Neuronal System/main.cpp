1cc5e59bcbfcf88de459d45e1b51c455563f0785
colinBranca <colin.branca@epfl.ch>
2017-10-03 15:52:59 +0200

    Update main

--- Neuronal System/main.cpp
+++ Neuronal System/main.cpp
@@ -14,20 +14,26 @@ int main() {
   double resistance = tau/conductivity;
   double h = 0.1;
   double current = 0.0;
+  double ext_i = 0.0;
   int tauRef = 2;
   double Vreset = -70.0;
   double Vthreshold = -55.0;
   double deltaTime = 0.0;
 
   std::cout << "insert current you want to apply:" << '\n';
-  std::cin >> current;
+  std::cin >> ext_i;
   std::cout << "insert duration of the current application" << '\n';
   std::cin >> deltaTime;
 
 
   neuron n = neuron(resistance, conductivity, tauRef, h, Vreset, Vthreshold);
 
-  for(double i = 0.0f; i <= deltaTime; i += h) {
+  for(double i = 0.0f; i <= 2.0*deltaTime; i += h) {
+    if(i >= deltaTime/2.0 && i<= 3.0*deltaTime/2.0) {
+      current = ext_i;
+    }
+    else current = 0.0;
+    
     n.updateState(i, current);
   }
 
