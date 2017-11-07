c9648fa94589ab697ba20481ad04660c72da37e9
colinBranca <colin.branca@epfl.ch>
2017-11-07 09:36:06 +0100

    Begin writting some tests

---
+++ neuron_unittest.cpp
@@ -0,0 +1,51 @@
+#include "neuron.h"
+#include "gtest/gtest.h"
+namespace {
+
+
+TEST(neuron, updateState_1) {
+  neuron* n = new neuron();
+  n->setCurrent(1000);
+  n->updateState(1);
+  EXPECT_EQ(true, n->updateState(2));
+}
+
+TEST(neuron, updateState_2) {
+  neuron* n = new neuron();
+  n->setCurrent(1000);
+  n->updateState(1);
+  n->updateState(2);
+  EXPECT_EQ(false, n->updateState(3));
+}
+
+TEST(neuron, updateState_3) {
+  neuron* n = new neuron();
+  n->setCurrent(1000);
+  n->updateState(1);
+  n->updateState(2);
+  n->updateState(3);
+  EXPECT_EQ(true, n->isRefractoring());
+}
+
+TEST(neuron, updateRefractoring) {
+  neuron* n = new neuron();
+  n->setCurrent(1000);
+  for(int i=0; i < 22; ++i) {
+    n -> updateState(i);
+  }
+  EXPECT_EQ(false, n->isRefractoring());
+}
+
+TEST(neuron, targets) {
+  neuron* n = new neuron();
+  std::vector<neuron*> targets;
+  for(int i=0; i< 5; ++i) {
+    neuron* ni = new neuron();
+    n->addTarget(ni);
+    targets.push_back(ni);
+  }
+  EXPECT_EQ(targets, n->getTargets());
+}
+
+
+}
