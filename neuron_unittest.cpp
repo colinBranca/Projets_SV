f215fb1a0fbd07a201f3203e9738cdab9c453a50
colinBranca <colin.branca@epfl.ch>
2017-11-08 15:29:10 +0100

    Add some tests

--- neuron_unittest.cpp
+++ neuron_unittest.cpp
@@ -3,22 +3,33 @@
 namespace {
 
 
-TEST(neuron, updateState_1) {
-  neuron* n = new neuron();
-  n->setCurrent(1000);
-  n->updateState(1);
-  EXPECT_EQ(true, n->updateState(2));
-}
+  TEST(neuron, updateState_1) {
+    neuron* n = new neuron();
+    n->setCurrent(0.9);
+    for(int i = 0; i < 500; ++i) {
+      n->updateState(i);
+    }
+    EXPECT_EQ(0, n->getSpikes());
+  }
 
 TEST(neuron, updateState_2) {
   neuron* n = new neuron();
-  n->setCurrent(1000);
+  n->setCurrent(1.1);
+  for(int i = 0; i < 500; ++i) {
+    n->updateState(i);
+  }
+  EXPECT_EQ(1, n->getSpikes());
+}
+
+TEST(neuron, updateState_3) {
+  neuron* n = new neuron();
+  n->setCurrent(1);
   n->updateState(1);
   n->updateState(2);
   EXPECT_EQ(false, n->updateState(3));
 }
 
-TEST(neuron, updateState_3) {
+TEST(neuron, updateState_4) {
   neuron* n = new neuron();
   n->setCurrent(1000);
   n->updateState(1);
@@ -45,6 +56,22 @@ TEST(neuron, targets) {
     targets.push_back(ni);
   }
   EXPECT_EQ(targets, n->getTargets());
+}
+
+TEST(neuron, receive) {
+  neuron* n = new neuron();
+  n->receive(0, 21);
+  for(int i=0; i<16; ++i) {
+    n->updateState(i);
+  }
+  EXPECT_EQ(true, n->updateState(16));
+}
+
+TEST(neuron, receiveFromExt) {
+  neuron* n = new neuron();
+  n->receiveFromExt(21);
+  n->updateState(1);
+  EXPECT_EQ(true, n->updateState(2));
 }
 
 
